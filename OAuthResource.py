9652c284e4554068006b65ce05e085aa5794366e
Konrad Budaj <k.budaj@dook.pro>
2017-06-08 14:09:51 +0200

    Divided into seperate files

---
+++ OAuthResource.py
@@ -0,0 +1,52 @@
+import falcon
+import json
+from requests_oauthlib import OAuth2Session
+
+
+class AuthResource(object):
+    def __init__(self, settings):
+        self.settings = settings
+        self.session_options = {}
+
+        possible_session_options = {'scope', 'redirect_uri'}
+        for key in settings:
+            if key in possible_session_options:
+                self.session_options[key] = settings[key]
+
+    def on_get(self, req, resp):
+        session = OAuth2Session(
+            self.settings['client_id'], **self.session_options)
+        authorization_url, state = session.authorization_url(
+            self.settings['auth_uri'])
+        raise falcon.HTTPMovedPermanently(authorization_url)
+
+
+class AuthCallbackResource(AuthResource):
+    def __init__(self, settings, callback):
+        super().__init__(settings)
+        self.callback = callback
+
+    def on_get(self, req, resp):
+        self.callback.response = resp
+        self.callback.request = req
+
+        auth_resp_url = 'https://' + req.url.split('//')[1]
+        session = OAuth2Session(
+            self.settings['client_id'], **self.session_options)
+        session.fetch_token(
+            self.settings['token_uri'],
+            client_secret=self.settings['client_secret'],
+            authorization_response=auth_resp_url)
+        data = session.get(self.settings['data_request_url'])
+        data = json.loads(data.content)
+
+        self.callback.execute(data)
+
+
+class Callback(object):
+    def __init__(self):
+        self.response = falcon.Response
+
+    def execute(self, oauth_data):
+        """ Interface like, requires implementation """
+        raise NotImplementedError("Execute method requires implementation")
---
+++ example.py
@@ -0,0 +1,50 @@
+import falcon
+import os
+import json
+
+from OAuthResource import AuthResource, AuthCallbackResource, Callback
+
+GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
+GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
+
+GITHUB_CLIENT_SECRET = os.environ['GITHUB_CLIENT_SECRET']
+GITHUB_CLIENT_ID = os.environ['GITHUB_CLIENT_ID']
+
+google_credentials = {
+    'client_id': GOOGLE_CLIENT_ID,
+    'client_secret': GOOGLE_CLIENT_SECRET,
+    'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+    'token_uri': 'https://accounts.google.com/o/oauth2/token',
+    'redirect_uri': 'http://127.0.0.1:8000/google/callback',
+    'scope': ['https://www.googleapis.com/auth/userinfo.email',
+              'https://www.googleapis.com/auth/userinfo.profile'],
+    'data_request_url': 'https://www.googleapis.com/oauth2/v1/userinfo'
+}
+
+github_credentials = {
+    'client_id': GITHUB_CLIENT_ID,
+    'client_secret': GITHUB_CLIENT_SECRET,
+    'auth_uri': 'https://github.com/login/oauth/authorize',
+    'token_uri': 'https://github.com/login/oauth/access_token',
+    'data_request_url': 'https://api.github.com/user'
+}
+
+
+class PrintUserID(Callback):
+    def execute(self, oauth_data):
+        self.response.status = falcon.HTTP_200
+        self.response.body = json.dumps(oauth_data)
+        print("-- Callback function, print_user_id --")
+        print("User ID: {}".format(oauth_data['id']))
+        print("--------------------------------------")
+
+
+app = falcon.API()
+
+app.add_route('/google/', AuthResource(google_credentials))
+app.add_route('/google/callback', AuthCallbackResource(
+    google_credentials, callback=PrintUserID()))
+
+app.add_route('/github/', AuthResource(github_credentials))
+app.add_route('/github/callback', AuthCallbackResource(
+    github_credentials, callback=PrintUserID()))
